36d69df779ec521b4024419bf3d224c18d46d393
B3rzxd3v <d3vilc0re@gmail.com>
2023-07-27 11:53:29 +0700

    Added Button and Input styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { styled } from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
+
+const H1 = styled.h1`
+  font-size: 30px;
+  text-transform: uppercase;
+  padding: 50px;
+  color: var(--color-brand-500);
+`;
+
 function App() {
-
-
   return (
     <>
-      <p>hello world</p>
+      <GlobalStyles />
+      <H1>hello world</H1>
+      <Button>Some</Button>
+      <Input placeholder='Enter here' />
     </>
   );
 }
