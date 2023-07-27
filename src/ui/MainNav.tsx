a572943d7d41e60a5f6827ba3cf88b262166e34e
B3rzxd3v <d3vilc0re@gmail.com>
2023-07-27 15:56:31 +0700

    Styled Sidebar

--- src/ui/MainNav.tsx
+++ src/ui/MainNav.tsx
@@ -1,4 +1,13 @@
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+import { APP_ROUTES } from '../const';
+import {
+  HiOutlineCalendarDays,
+  HiOutlineCog6Tooth,
+  HiOutlineHome,
+  HiOutlineHomeModern,
+  HiOutlineUsers,
+} from 'react-icons/hi2';
 
 const NavList = styled.ul`
   display: flex;
@@ -6,7 +15,7 @@ const NavList = styled.ul`
   gap: 0.8rem;
 `;
 
-const Link = styled.a`
+const Link = styled(NavLink)`
   &:link,
   &:visited {
     display: flex;
@@ -44,3 +53,44 @@ const Link = styled.a`
     color: var(--color-brand-600);
   }
 `;
+
+function MainNav() {
+  return (
+    <nav>
+      <NavList>
+        <li>
+          <Link to={APP_ROUTES.DASHBOARD}>
+            <HiOutlineHome />
+            <span>Home</span>
+          </Link>
+        </li>
+        <li>
+          <Link to={APP_ROUTES.BOOKINGS}>
+            <HiOutlineCalendarDays />
+            <span>Bookings</span>
+          </Link>
+        </li>
+        <li>
+          <Link to={APP_ROUTES.CABINS}>
+            <HiOutlineHomeModern />
+            <span>Cabins</span>
+          </Link>
+        </li>
+        <li>
+          <Link to={APP_ROUTES.USERS}>
+            <HiOutlineUsers />
+            <span>Users</span>
+          </Link>
+        </li>
+        <li>
+          <Link to={APP_ROUTES.SETTINGS}>
+            <HiOutlineCog6Tooth />
+            <span>Settings</span>
+          </Link>
+        </li>
+      </NavList>
+    </nav>
+  );
+}
+
+export default MainNav;
--- src/ui/Sidebar.tsx
+++ src/ui/Sidebar.tsx
@@ -1,14 +1,24 @@
 import { styled } from 'styled-components';
+import Logo from './Logo';
+import MainNav from './MainNav';
 
 const Aside = styled.aside`
   padding: 3.2rem 2.4rem;
   border-right: 1px solid var(--color-grey-100);
   background-color: var(--color-grey-0);
   grid-row: 1 / -1;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 
 function Sidebar() {
-  return <Aside>Sidebar</Aside>;
+  return (
+    <Aside>
+      <Logo />
+      <MainNav />
+    </Aside>
+  );
 }
 
 export default Sidebar;
