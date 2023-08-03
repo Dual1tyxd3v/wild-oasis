73872e32883a12ab1bd2b5974c21e1ef6ed83c08
B3rzxd3v <d3vilc0re@gmail.com>
2023-08-03 18:23:37 +0700

    Wrapped APP into Error Boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from './ui/ErrorFallback.tsx';
+import { APP_ROUTES } from './const.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace(APP_ROUTES.MAIN)}
+    >
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
